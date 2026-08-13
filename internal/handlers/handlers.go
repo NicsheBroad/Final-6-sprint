ab7ea9431713e2a001813e63282a3e1b925ea34e
Admin <tmshnk0@yandex.ru>
2026-08-14 03:36:28 +0500

    5 commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -60,11 +59,8 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 
 	ext := filepath.Ext(header.Filename)
 
-	filename := fmt.Sprintf(
-		"converted_%s%s",
-		time.Now().UTC().Format("20060102_150405"),
-		ext,
-	)
+	filename := time.Now().UTC().String() + ext
+
 	output, err := os.Create(filename)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
